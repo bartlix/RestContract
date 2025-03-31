628be67e0fbaff602f7b6e543ea95d4dc805f180
Raphael Schwarz <raphael@codeworx.at>
2025-03-31 17:57:39 +0200

    response type test

--- test/Codeworx.Rest.IntegrationTests/ResponseTypeSwaggerTest.cs
+++ test/Codeworx.Rest.IntegrationTests/ResponseTypeSwaggerTest.cs
@@ -32,5 +32,28 @@ namespace Codeworx.Rest.UnitTests
 
         }
 
+        [Fact]
+        public async Task TestDownloadSwaggerFileHasResponseTypeMediaTypeForOKDefaultResponse()
+        {
+            var request = TestServer.CreateRequest("swagger/v1/swagger.json");
+            var response = await request.SendAsync(HttpMethods.Get);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var swagger = await JsonSerializer.DeserializeAsync<Dictionary<string, object>>(
+                await response.Content.ReadAsStreamAsync());
+
+            var paths = (JsonElement)swagger["paths"];
+
+            var responses = paths.GetProperty("/api/StatusCode/Data").GetProperty("get").GetProperty("responses");
+
+            Assert.True(responses.TryGetProperty("200", out var response1));
+
+            Assert.True(responses.TryGetProperty("409", out var response2));
+
+            Assert.True(responses.TryGetProperty("410", out var response3));
+
+        }
+
     }
 }
---
+++ test/Codeworx.Rest.IntegrationTests/TestServerUtilities/CustomStringOutputFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Codeworx.Rest.UnitTests.TestServerUtilities
+{
+    public class CustomStringOutputFormatter : StringOutputFormatter
+    {
+        protected override bool CanWriteType(Type type)
+        {
+            if (type == typeof(string))
+            {
+                // Call into base to check if the current request's content type is a supported media type.
+                return base.CanWriteType(type);
+            }
+
+            return false;
+        }
+    }
+}
